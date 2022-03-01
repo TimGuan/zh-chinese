a92185b547d2b6de9b63739f6f91db0866a19c0f
Daniel Kirchner <daniel@ekpyron.org>
2022-03-01 18:28:05 +0100

    Adjust tests to warnings.

--- test/libsolidity/syntaxTests/inlineAssembly/assembly_invalid_options.sol
+++ test/libsolidity/syntaxTests/inlineAssembly/assembly_invalid_options.sol
@@ -2,7 +2,7 @@ function f() pure {
     assembly ("a", "b", "c", "c") {}
 }
 // ----
-// SyntaxError 4430: (24-56): Unexpected inline assembly flag: "a"
-// SyntaxError 4430: (24-56): Unexpected inline assembly flag: "b"
-// SyntaxError 4430: (24-56): Unexpected inline assembly flag: "c"
-// SyntaxError 4430: (24-56): Unexpected inline assembly flag: "c"
+// Warning 4430: (24-56): Unknown inline assembly flag: "a"
+// Warning 4430: (24-56): Unknown inline assembly flag: "b"
+// Warning 4430: (24-56): Unknown inline assembly flag: "c"
+// Warning 4430: (24-56): Unknown inline assembly flag: "c"
